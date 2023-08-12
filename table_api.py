0abad8f3b43ff52243a0a700aa8793498979cb35
Shreyas Bhatkande <shreyas@Ravindras-MacBook-Pro.local>
2023-08-12 01:05:28 -0400

    added check quiz

--- table_api.py
+++ table_api.py
@@ -1,12 +1,15 @@
 import boto3
 import datetime;
 import json
+from boto3.dynamodb.conditions import Key
+
 
 db_resource = boto3.resource('dynamodb', aws_access_key_id="anything", aws_secret_access_key="anything", 
                           region_name='us-east-1', endpoint_url="http://localhost:8000")
 db_client = boto3.client("dynamodb", aws_access_key_id="anything", aws_secret_access_key="anything", 
                           region_name='us-east-1', endpoint_url="http://localhost:8000")
 table = db_resource.Table('Quizzes')
+
 
 def print_table(table_name):
     response = db_client.scan(TableName=table_name)
@@ -43,6 +46,7 @@ def create_new_quiz(input_json, output_json):
     out_json = {'quiz_id': quiz_id, 'quiz_name': quiz_name}
     json.dump(out_json, fp=output_json)
 
+
 def get_quizzes(output_json):
     response = db_client.scan(TableName='Quizzes', Select='SPECIFIC_ATTRIBUTES', ProjectionExpression='quiz_id,quiz_name')
     out_dict = {}
@@ -55,9 +59,35 @@ def get_quizzes(output_json):
             out_dict[list(r['quiz_id'].values())[0]] = list(r['quiz_name'].values())[0]
     json.dump(out_dict, fp=output_json)
     
+    
+def check_quiz(input_json, output_json):
+    inp_dict = json.load(input_json)
+    response = table.query(Select="SPECIFIC_ATTRIBUTES", ProjectionExpression='answer,question_number', 
+                               KeyConditionExpression=Key('quiz_id').eq(inp_dict['quiz_id']))
+    qa_pairs = {}
+    for item in response['Items']:
+        qa_pairs[int(item['question_number'])] = int(item['answer'])
+    tot_answers = len(qa_pairs)
+    correct_answers = 0
+    wrong_answers = 0
+    out_list = []
+    for index, answer in enumerate(inp_dict['Answers']):
+        if qa_pairs[index + 1] == answer:
+            out_list.append(True)
+            correct_answers += 1
+        else:
+            out_list.append(False)
+            wrong_answers += 1
+    out_dict = {'tot_answers': tot_answers, 'correct_answers': correct_answers, 'wrong_answers': wrong_answers, 'record': out_list}
+    json.dump(out_dict, fp=output_json)
+    
+    
 output_file = open('out.json', 'w')
 add_quiz_input = open('add_quiz_data.json')
+check_quiz_input = open('check_quiz_data.json')
 # create_new_quiz(add_quiz_input, output_file)
 # get_quizzes(output_file)
+# check_quiz(check_quiz_input, output_file)
 output_file.close()
 add_quiz_input.close()
+check_quiz_input.close()
