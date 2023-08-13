06f9490442825d74d64be72d1ae52ac0ff7b198c
Shreyas Bhatkande <shreyas@Ravindras-MacBook-Pro.local>
2023-08-12 21:21:06 -0400

    added change name

--- table_api.py
+++ table_api.py
@@ -175,22 +175,46 @@ def change_answer(input_json, output_json):
     json.dump(out_dict, output_json)
     
     
+def change_name(input_json, output_json):
+    inp_dict = json.load(input_json)
+    response = quiz_table.query(Select="SPECIFIC_ATTRIBUTES", ProjectionExpression='question_number,quiz_name', 
+                               KeyConditionExpression=Key('quiz_id').eq(inp_dict['quiz_id']))
+    old_name = ""
+    for item in response['Items']:
+        old_name = item['quiz_name']
+        key_exp = Key('quiz_id').eq(inp_dict['quiz_id'])
+        key_exp &= Key('question_number').eq(item['question_number'])
+        quiz_table.update_item(Key={'quiz_id': inp_dict['quiz_id'], 'question_number': item['question_number']},
+                           UpdateExpression='set quiz_name = :n',
+                           ExpressionAttributeValues={
+                               ":n": inp_dict['new_name']
+                           })
+    out_dict = {}
+    out_dict['name'] = inp_dict['new_name']
+    out_dict['old_name'] = old_name
+    out_dict['quiz_id'] = inp_dict['quiz_id']
+    json.dump(out_dict, output_json)
+    
+    
 output_file = open('out.json', 'w')
 add_quiz_input = open('add_quiz_data.json')
 check_quiz_input = open('check_quiz_data.json')
 add_choice_input = open('add_choice_data.json')
 remove_choice_input = open('remove_choice_data.json')
 change_answer_input = open('change_answer_data.json')
+change_name_input = open('change_name_data.json')
 # print_table('Quizzes')
 # create_new_quiz(add_quiz_input, output_file)
 # get_quizzes(output_file)
 # check_quiz(check_quiz_input, output_file)
 # add_choice(add_choice_input, output_file)
 # remove_choice(remove_choice_input, output_file)
-change_answer(change_answer_input, output_file)
+# change_answer(change_answer_input, output_file)
+change_name(change_name_input, output_file)
 output_file.close()
 add_quiz_input.close()
 check_quiz_input.close()
 add_choice_input.close()
 remove_choice_input.close()
 change_answer_input.close()
+change_name_input.close()
